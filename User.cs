0b49e8e351dccbe7079b28076bf84dfb8c19c414
wathanyuka-eng <wathanyu.ka@kkumail.com>
2026-03-17 11:46:26 +0700

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,14 @@
+﻿namespace GradeSystem
+{
+    abstract class User
+    {
+        public string Name { get; set; }
+
+        public User(string name)
+        {
+            Name = name;
+        }
+
+        public abstract string GetInfo();
+    }
+}
